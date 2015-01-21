837514fc01d7b3df1ae473ec00c4009a91ab12bc
Dylan Etkin <dylan.etkin@me.com>
2015-01-21 15:48:39 -0800

    Add the post method so we can create events

--- event_service/views.py
+++ event_service/views.py
@@ -3,19 +3,21 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from event_service.models import Event, Organization
 from event_service.serializers import EventSerializer
+from dateutil import parser
 
 
 class EventsList(APIView):
 
     def get(self, request, *args, **kwargs):
-        org_name = kwargs.get('org', None)
-        if not org_name or not Organization.objects.filter(name=org_name).exists():
+
+        # Get the org first
+        org = self._get_org(kwargs)
+        if not org:
             return Response('Org not found',
                             status=status.HTTP_404_NOT_FOUND)
 
         # Setup the base query, filter by org and always have desc created
         # sorting
-        org = Organization.objects.get(name=org_name)
         query = Event.objects.filter(org=org).order_by('-created_on')
 
         # filter by hostname if it has been provided
@@ -33,3 +35,42 @@ class EventsList(APIView):
 
         serializer = EventSerializer(query[0:page_size], many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+
+        # Get the org first
+        org = self._get_org(kwargs)
+        if not org:
+            return Response('Org not found',
+                            status=status.HTTP_404_NOT_FOUND)
+
+        # Verify we have the data for the event
+        if ('hostname' not in request.data or
+            'string' not in request.data or
+            'created_on' not in request.data):
+            return Response('Event data not provided correctly',
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Convert this to a date
+        try:
+            created_on = parser.parse(request.data['created_on'])
+        except ValueError:
+            return Response('Malformed date',
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Let's create an event
+        e = Event.objects.create(org=org, hostname=request.data['hostname'],
+                                 string=request.data['string'],
+                                 created_on=created_on)
+
+        # Send the created event representation back to the user
+        serializer = EventSerializer(e)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def _get_org(self, kwargs):
+        org_name = kwargs.get('org', None)
+        if not org_name or not Organization.objects.filter(name=org_name).exists():
+            return None
+
+        return Organization.objects.get(name=org_name)
+
